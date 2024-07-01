ca6006eff60272ad087989e1063da6c10f560360
Divyalochan Mangaraj <111107629+Divya933d@users.noreply.github.com>
2024-07-01 11:35:30 +0530

    Add files via upload

---
+++ Card.jsx
@@ -0,0 +1,17 @@
+
+import './styles.css'; 
+const Card = (props) => {
+    const { title, imageUrl, description } = props;
+
+    return (
+        <div className="card">
+            <img src={imageUrl} alt={title} className="card-image" />
+            <div className="card-content">
+                <h2 className="card-title">{title}</h2>
+                <p className="card-description">{description}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
